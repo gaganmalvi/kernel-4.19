c0663448dbc05142408276663a5dbf4b69c97177
Gagan Malvi <malvi@aospa.co>
2021-07-05 05:39:33 +0200

    net: Import changes from A125FXXU1BUE3.
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- net/ipv6/ndisc.c
+++ net/ipv6/ndisc.c
@@ -73,6 +73,12 @@
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv6.h>
 
+#ifdef CONFIG_MTK_SRIL_SUPPORT
+#define NET_IF_NAME	"rmnet"
+#else
+#define NET_IF_NAME	"ccmni"
+#endif
+
 static u32 ndisc_hash(const void *pkey,
 		      const struct net_device *dev,
 		      __u32 *hash_rnd);
@@ -197,6 +203,8 @@ static inline int ndisc_is_useropt(const struct net_device *dev,
 {
 	return opt->nd_opt_type == ND_OPT_RDNSS ||
 		opt->nd_opt_type == ND_OPT_DNSSL ||
+		opt->nd_opt_type == ND_OPT_CAPTIVE_PORTAL ||
+		opt->nd_opt_type == ND_OPT_PREF64 ||
 		ndisc_ops_is_useropt(dev, opt->nd_opt_type);
 }
 
@@ -1227,6 +1235,14 @@ static void ndisc_router_discovery(struct sk_buff *skb)
 		 *	out on this interface.
 		 */
 		in6_dev->if_flags |= IF_RA_RCVD;
+	}
+
+	if ((sysctl_optr == MTK_IPV6_VZW_ALL ||
+	    sysctl_optr == MTK_IPV6_EX_RS_INTERVAL) &&
+	    (strncmp(in6_dev->dev->name, NET_IF_NAME, 2) == 0)) {
+		/*add for VzW feature : remove IF_RS_VZW_SENT flag*/
+		if (in6_dev->if_flags & IF_RS_VZW_SENT)
+			in6_dev->if_flags &= ~IF_RS_VZW_SENT;
 	}
 
 	/*
